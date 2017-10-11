6ebad2ef820016a7d0974ef2e17ed1ff0bdff54d
David Ribeiro Lopes <dlopes@dlopes-mac.local>
2017-10-11 17:56:39 -0300

    Adds examples

---
+++ examples/allbts.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dlopes7/go-appdynamics-rest-api/appdrest"
+)
+
+func main() {
+	client := appdrest.NewClient("http", "192.168.33.10", 8090, "admin", "password", "customer1")
+
+	apps, err := client.Application.GetApplications()
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, app := range apps {
+
+		bts, err := client.BusinessTransaction.GetBusinessTransactions(app.ID)
+		if err != nil {
+			panic(err.Error())
+		}
+		for _, bt := range bts {
+			fmt.Printf("Tier: %s, BT: %s, Type: %s\n", bt.TierName, bt.Name, bt.EntryPointType)
+		}
+
+	}
+
+}
