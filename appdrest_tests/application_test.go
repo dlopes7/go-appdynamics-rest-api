cb41b7c0fed1796d9929cc3f812bf469d472726e
David Ribeiro Lopes <dlopes@dlopes-mac.local>
2017-11-19 21:53:49 -0200

    Add test for Application

--- appdrest_tests/application_test.go
+++ appdrest_tests/application_test.go
@@ -1,6 +1,7 @@
 package appdrest_tests
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/dlopes7/go-appdynamics-rest-api/appdrest"
@@ -14,8 +15,29 @@ func TestGetApplications(t *testing.T) {
 	apps, err := client.Application.GetApplications()
 	if err != nil {
 		t.Errorf("Error getting applications: %s\n", err.Error())
+		t.FailNow()
 	}
 	if len(apps) == 0 {
 		t.Error("No applications were returned")
+	}
+}
+
+func TestGetApplication(t *testing.T) {
+	apps, err := client.Application.GetApplications()
+	if err != nil {
+		t.Errorf("Error getting applications: %s\n", err.Error())
+		t.FailNow()
+	}
+	if len(apps) == 0 {
+		t.Error("No applications were returned")
+	}
+
+	app, err := client.Application.GetApplication(strconv.Itoa(apps[0].ID))
+	if err != nil {
+		t.Errorf("Error getting Application %s", apps[0].Name)
+		t.FailNow()
+	}
+	if len(app.Name) == 0 || app.ID <= 0 {
+		t.Errorf("Error getting Application properties for %s", apps[0].Name)
 	}
 }
