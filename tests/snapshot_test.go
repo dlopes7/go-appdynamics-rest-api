4878e808e3efba40c9e5b8bfe1dbe15737b87bbb
David Lopes <davidribeirolopes@gmail.com>
2018-05-04 13:17:10 -0300

    Adds snapshots tests

---
+++ tests/snapshot_test.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/dlopes7/go-appdynamics-rest-api/appdrest"
+
+	"os"
+)
+
+func TestGetSnapshots(t *testing.T) {
+	port, _ := strconv.Atoi(os.Getenv("APPD_CONTROLLER_PORT"))
+	client := appdrest.NewClient(os.Getenv("APPD_CONTROLLER_PROTOCOL"), os.Getenv("APPD_CONTROLLER_HOST"), port, os.Getenv("APPD_USER"), os.Getenv("APPD_PASSWORD"), os.Getenv("APPD_ACCOUNT"))
+
+	apps, err := client.Application.GetApplications()
+	if err != nil {
+		t.Errorf("Error getting apps: %s\n", err.Error())
+		t.FailNow()
+	}
+	if len(apps) > 0 {
+		app := apps[0]
+		filters := &appdrest.SnapshotFilters{}
+		snapshots, err := client.Snapshot.GetSnapshots(app.ID, appdrest.TimeBEFORENOW, 15, time.Now(), time.Now(), filters)
+		if err != nil {
+			t.Errorf("Error getting snapshots: %s\n", err.Error())
+			t.FailNow()
+		}
+		t.Logf("Got %d snapshots from the app %s", len(snapshots), app.Name)
+
+	}
+
+}
