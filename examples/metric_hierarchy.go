38442255afd0d0e945b919b8303b61a0695c681c
David Lopes <davidribeirolopes@gmail.com>
2018-05-06 00:12:12 -0300

    Adds a metric hierarchy example

---
+++ examples/metric_hierarchy.go
@@ -0,0 +1,48 @@
+package examples
+
+/*
+This example will list every single metric for an Application
+This works with a recursive call to the GetMetricHierarchy function
+*/
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/dlopes7/go-appdynamics-rest-api/appdrest"
+)
+
+func recursivePath(client *appdrest.Client, appName string, prefix string) {
+	metricH, err := client.MetricData.GetMetricHierarchy(appName, prefix)
+	if err != nil {
+		panic(err.Error())
+	}
+	if prefix != "" {
+		prefix = prefix + "|"
+	}
+
+	for _, metric := range metricH {
+		if metric.Type == "folder" {
+			// If we are in a folder, go down one more level
+			recursivePath(client, appName, prefix+metric.Name)
+		} else {
+			// If we are here we got to the bottom, we have a metric
+			fmt.Println(prefix + metric.Name)
+		}
+	}
+
+}
+
+func printMetricHierarchy() {
+	port, _ := strconv.Atoi(os.Getenv("APPD_CONTROLLER_PORT"))
+	client := appdrest.NewClient(os.Getenv("APPD_CONTROLLER_PROTOCOL"), os.Getenv("APPD_CONTROLLER_HOST"), port, os.Getenv("APPD_USER"), os.Getenv("APPD_PASSWORD"), os.Getenv("APPD_ACCOUNT"))
+
+	apps, err := client.Application.GetApplications()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	app := apps[0]
+	recursivePath(client, app.Name, "")
+}
