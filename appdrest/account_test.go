08fcb63959b7c6f47e64351ac0e7d57b20a52412
David Lopes <davidribeirolopes@gmail.com>
2018-05-06 11:10:36 -0300

    Add tests to account_test.go

--- appdrest/account_test.go
+++ appdrest/account_test.go
@@ -3,6 +3,7 @@ package appdrest_test
 import (
 	"os"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/dlopes7/go-appdynamics-rest-api/appdrest"
@@ -37,5 +38,66 @@ func TestGetLicenseModules(t *testing.T) {
 		t.FailNow()
 	}
 	t.Logf("Got properties for %d license modules", len(modules))
+
+	_, err = client.Account.GetLicenseModules("-1")
+	if err != nil {
+		t.Logf("Expeceted error getting license Modules for invalid Account: %s\n", err.Error())
+	}
+
+}
+
+func TestGetLicenseProperties(t *testing.T) {
+	port, _ := strconv.Atoi(os.Getenv("APPD_CONTROLLER_PORT"))
+	client := appdrest.NewClient(os.Getenv("APPD_CONTROLLER_PROTOCOL"), os.Getenv("APPD_CONTROLLER_HOST"), port, os.Getenv("APPD_USER"), os.Getenv("APPD_PASSWORD"), os.Getenv("APPD_ACCOUNT"))
+
+	acc, err := client.Account.GetMyAccount()
+	if err != nil {
+		t.Errorf("Error getting account: %s\n", err.Error())
+		t.FailNow()
+	}
+	if len(acc.Name) == 0 {
+		t.Errorf("Error getting account name: %v\n", acc)
+		t.FailNow()
+	}
+
+	properties, err := client.Account.GetLicenseProperties(acc.ID, "apm")
+	if err != nil {
+		t.Errorf("Error getting license Properties: %s\n", err.Error())
+		t.FailNow()
+	}
+
+	t.Logf("Agent type APM, %s: %s", properties[2].Name, properties[2].Value)
+
+	_, err = client.Account.GetLicenseProperties(acc.ID, "doesnt_exist")
+	if err != nil {
+		t.Logf("Expected error when getting invalid license Properties: %s\n", err.Error())
+	}
+}
+
+func TestGetLicenseUsages(t *testing.T) {
+	port, _ := strconv.Atoi(os.Getenv("APPD_CONTROLLER_PORT"))
+	client := appdrest.NewClient(os.Getenv("APPD_CONTROLLER_PROTOCOL"), os.Getenv("APPD_CONTROLLER_HOST"), port, os.Getenv("APPD_USER"), os.Getenv("APPD_PASSWORD"), os.Getenv("APPD_ACCOUNT"))
+
+	acc, err := client.Account.GetMyAccount()
+	if err != nil {
+		t.Errorf("Error getting account: %s\n", err.Error())
+		t.FailNow()
+	}
+	if len(acc.Name) == 0 {
+		t.Errorf("Error getting account name: %v\n", acc)
+		t.FailNow()
+	}
+
+	usages, err := client.Account.GetLicenseUsages(acc.ID, "apm")
+	if err != nil {
+		if strings.Contains(err.Error(), "500") {
+			t.Logf("User doesn't have permission to check license usage")
+		} else {
+			t.Errorf("Error getting license Usages: %s\n", err.Error())
+		}
+	}
+	for usage := range usages {
+		t.Logf("Usage %+v", usage)
+	}
 
 }
