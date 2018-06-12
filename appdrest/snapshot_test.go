85d685f4b38331b34eae867155fd2b678f08bf40
David Lopes <davidribeirolopes@gmail.com>
2018-06-12 09:52:46 -0300

    Add more tests to Snapshots

--- appdrest/snapshot_test.go
+++ appdrest/snapshot_test.go
@@ -30,3 +30,30 @@ func TestGetSnapshots(t *testing.T) {
 	}
 
 }
+
+func TestGetSnapshotsFiltered(t *testing.T) {
+	port, _ := strconv.Atoi(os.Getenv("APPD_CONTROLLER_PORT"))
+	client, _ := appdrest.NewClient(os.Getenv("APPD_CONTROLLER_PROTOCOL"), os.Getenv("APPD_CONTROLLER_HOST"), port, os.Getenv("APPD_USER"), os.Getenv("APPD_PASSWORD"), os.Getenv("APPD_ACCOUNT"))
+	apps, err := client.Application.GetApplications()
+	if err != nil {
+		t.Errorf("Error getting apps: %s\n", err.Error())
+		t.FailNow()
+	}
+	if len(apps) > 0 {
+		app := apps[0]
+		filters := &appdrest.SnapshotFilters{
+			ExecutionTimeInMilis: 3000,
+		}
+		then := time.Now().Add(time.Duration(-20) * time.Minute)
+		thenMinus5 := time.Now().Add(time.Duration(-5) * time.Minute)
+
+		snapshots, err := client.Snapshot.GetSnapshots(app.ID, appdrest.TimeAFTERTIME, 15, then, thenMinus5, filters)
+		if err != nil {
+			t.Errorf("Error getting snapshots: %s\n", err.Error())
+			t.FailNow()
+		}
+		t.Logf("Got %d snapshots from app %s", len(snapshots), app.Name)
+
+	}
+
+}
